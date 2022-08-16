ba6c65c2a0324de22a11d194c83cb15ef81ba880
Luca Denti <ldenti@proton.me>
2022-08-16 13:39:06 +0200

    Adds script to summarize results

---
+++ summarize_results.py
@@ -0,0 +1,19 @@
+import sys
+import os
+import glob
+import pandas as pd
+
+def main():
+    indir = sys.argv[1]
+    data = []
+    for csv in glob.glob(os.path.join(indir, "*", "*", "ASGAL", "events.wpsi.csv")):
+        sample = csv.split("/")[-4]
+        chrom, info, w1, w2, w3, psi = open(csv).readline().strip("\n").split(",")
+        gene, etype, _chrom, I1, I2, strand = info.split("_")
+        data.append([sample, chrom, gene, etype, f"{I1}-{I2}", strand, w1, w2, w3, psi])
+
+    df = pd.DataFrame(data, columns = ["Sample", "Chromosome", "Gene", "Event", "Introns", "Strand", "W1", "W2", "W3", "PSI"])
+    print(df)
+
+if __name__ == "__main__":
+    main()
