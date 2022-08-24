ab3109cd5a0a540bcc09ca0262454d65cfe24de8
Luca Denti <ldenti92@gmail.com>
2022-08-24 08:57:26 +0200

    Avoids iterating over the entire GTF for every event

--- extract_subannotations.py
+++ extract_subannotations.py
@@ -21,7 +21,7 @@ def open_gtf(gtf_path):
     return gtf
 
 
-def analyze_ES(gtf, fpath):
+def analyze_ES(exons, fpath):
     for line in open(fpath):
         if line.startswith("seqname"):
             continue
@@ -38,22 +38,21 @@ def analyze_ES(gtf, fpath):
             and intron2[0] < intron2[1]
         )
         pre, mid, post = (0, 0), (0, 0), (0, 0)
-        for exon in gtf.features_of_type("exon"):
-            if exon.chrom == chrom and exon.attributes["gene_id"][0] == gene:
-                if exon.end == intron1[0]:
-                    if pre != (0, 0):
-                        if exon.start < pre[0]:
-                            pre = (exon.start, exon.end)
-                    else:
-                        pre = (exon.start, exon.end)
-                elif exon.start == intron1[1] and exon.end == intron2[0]:
-                    mid = (exon.start, exon.end)
-                elif exon.start == intron2[1]:
-                    if post != (0, 0):
-                        if exon.end > post[1]:
-                            post = (exon.start, exon.end)
-                    else:
-                        post = (exon.start, exon.end)
+        for (start, end) in exons[chrom][gene]:
+            if end == intron1[0]:
+                if pre != (0, 0):
+                    if start < pre[0]:
+                        pre = (start, end)
+                else:
+                    pre = (start, end)
+            elif start == intron1[1] and end == intron2[0]:
+                mid = (start, end)
+            elif start == intron2[1]:
+                if post != (0, 0):
+                    if end > post[1]:
+                        post = (start, end)
+                else:
+                    post = (start, end)
         print(
             chrom,
             ".",
@@ -120,108 +119,7 @@ def analyze_ES(gtf, fpath):
             )
 
 
-def analyze_A3(gtf, fpath):
-    for line in open(fpath):
-        if line.startswith("seqname"):
-            continue
-        idx = line.strip("\n").split("\t")[2]
-        gene, rest = idx.split(";")
-        _etype, chrom, intron1, intron2, strand = rest.split(":")
-        intron1 = (int(intron1.split("-")[0]), int(intron1.split("-")[1]))
-        intron2 = (int(intron2.split("-")[0]), int(intron2.split("-")[1]))
-        idx = idx.replace(";", "_")
-        idx = idx.replace(":", "_")
-        assert intron1[1] == intron2[1]
-        pre1, pre2, post = (0, 0), (0, 0), (0, 0)
-        for exon in gtf.features_of_type("exon"):
-            if exon.chrom == chrom and exon.attributes["gene_id"][0] == gene:
-                if exon.start == intron1[1]:
-                    if post != (0, 0):
-                        if exon.end > post[1]:
-                            post = (exon.start, exon.end)
-                    else:
-                        post = (exon.start, exon.end)
-                elif exon.end == intron1[0]:
-                    if pre1 != (0, 0):
-                        if exon.start < pre1[0]:
-                            pre1 = (exon.start, exon.end)
-                    else:
-                        pre1 = (exon.start, exon.end)
-                elif exon.end == intron2[0]:
-                    if pre2 != (0, 0):
-                        if exon.start < pre2[0]:
-                            pre2 = (exon.start, exon.end)
-                    else:
-                        pre2 = (exon.start, exon.end)
-        lpre = min([pre1, pre2], key=lambda x: x[0])
-        spre = max([pre1, pre2], key=lambda x: x[0])
-        print(
-            chrom,
-            ".",
-            "gene",
-            lpre[0],
-            post[1],
-            ".",
-            "+",
-            ".",
-            f'gene_id "{idx}_G";',
-            sep="\t",
-        )
-        print(
-            chrom,
-            ".",
-            "transcript",
-            lpre[0],
-            post[1],
-            ".",
-            "+",
-            ".",
-            f'gene_id "{idx}_G"; transcript_id "{idx}_T1"',
-            sep="\t",
-        )
-        for i, (s, e) in enumerate([lpre, post], 1):
-            print(
-                chrom,
-                ".",
-                "exon",
-                s,
-                e,
-                ".",
-                "+",
-                ".",
-                f'gene_id "{idx}_G"; transcript_id "{idx}_T1"; exon_id "{idx}_E{i}"',
-                sep="\t",
-            )
-        print(
-            chrom,
-            ".",
-            "transcript",
-            spre[0],
-            post[1],
-            ".",
-            "+",
-            ".",
-            f'gene_id "{idx}_G"; transcript_id "{idx}_T2"',
-            sep="\t",
-        )
-        for i, (s, e) in enumerate([spre, post], 1):
-            if i == 1:
-                i = 3
-            print(
-                chrom,
-                ".",
-                "exon",
-                s,
-                e,
-                ".",
-                "+",
-                ".",
-                f'gene_id "{idx}_G"; transcript_id "{idx}_T2"; exon_id "{idx}_E{i}"',
-                sep="\t",
-            )
-
-
-def analyze_SS(gtf, fpath):
+def analyze_SS(exons, fpath):
     for line in open(fpath):
         if line.startswith("seqname"):
             continue
@@ -240,46 +138,45 @@ def analyze_SS(gtf, fpath):
             assert intron1[0] == intron2[0]
 
         alt1, alt2, const = (0, 0), (0, 0), (0, 0)
-        for exon in gtf.features_of_type("exon"):
-            if exon.chrom == chrom and exon.attributes["gene_id"][0] == gene:
-                if mode:
-                    if exon.start == intron1[1]:
-                        if const != (0, 0):
-                            if exon.end > const[1]:
-                                const = (exon.start, exon.end)
-                        else:
-                            const = (exon.start, exon.end)
-                    elif exon.end == intron1[0]:
-                        if alt1 != (0, 0):
-                            if exon.start < alt1[0]:
-                                alt1 = (exon.start, exon.end)
-                        else:
-                            alt1 = (exon.start, exon.end)
-                    elif exon.end == intron2[0]:
-                        if alt2 != (0, 0):
-                            if exon.start < alt2[0]:
-                                alt2 = (exon.start, exon.end)
-                        else:
-                            alt2 = (exon.start, exon.end)
-                else:
-                    if exon.end == intron1[0]:
-                        if const != (0, 0):
-                            if exon.start < const[0]:
-                                const = (exon.start, exon.end)
-                        else:
-                            const = (exon.start, exon.end)
-                    elif exon.start == intron1[1]:
-                        if alt1 != (0, 0):
-                            if exon.end > alt1[1]:
-                                alt1 = (exon.start, exon.end)
-                        else:
-                            alt1 = (exon.start, exon.end)
-                    elif exon.start == intron2[1]:
-                        if alt2 != (0, 0):
-                            if exon.end > alt2[1]:
-                                alt2 = (exon.start, exon.end)
-                        else:
-                            alt2 = (exon.start, exon.end)
+        for (start, end) in exons[chrom][gene]:
+            if mode:
+                if start == intron1[1]:
+                    if const != (0, 0):
+                        if end > const[1]:
+                            const = (start, end)
+                    else:
+                        const = (start, end)
+                elif end == intron1[0]:
+                    if alt1 != (0, 0):
+                        if start < alt1[0]:
+                            alt1 = (start, end)
+                    else:
+                        alt1 = (start, end)
+                elif end == intron2[0]:
+                    if alt2 != (0, 0):
+                        if start < alt2[0]:
+                            alt2 = (start, end)
+                    else:
+                        alt2 = (start, end)
+            else:
+                if end == intron1[0]:
+                    if const != (0, 0):
+                        if start < const[0]:
+                            const = (start, end)
+                    else:
+                        const = (start, end)
+                elif start == intron1[1]:
+                    if alt1 != (0, 0):
+                        if end > alt1[1]:
+                            alt1 = (start, end)
+                    else:
+                        alt1 = (start, end)
+                elif start == intron2[1]:
+                    if alt2 != (0, 0):
+                        if end > alt2[1]:
+                            alt2 = (start, end)
+                    else:
+                        alt2 = (start, end)
 
         Ts = {}
         begin, end = 0, 0
@@ -337,10 +234,23 @@ def main():
     indir = sys.argv[2]
 
     gtf = open_gtf(gtf_path)
+    print("Extracting exons..", file=sys.stderr)
+    exons = {}
+    for exon in gtf.features_of_type("exon"):
+        chrom = exon.chrom
+        gidx = exon.attributes["gene_id"][0]
+        if chrom not in exons:
+            exons[chrom] = {}
+        if gidx not in exons[chrom]:
+            exons[chrom][gidx] = set()
+        exons[chrom][gidx].add((exon.start, exon.end))
 
-    analyze_ES(gtf, os.path.join(indir, "_SE_strict.ioe"))
-    analyze_SS(gtf, os.path.join(indir, "_A3_strict.ioe"))
-    analyze_SS(gtf, os.path.join(indir, "_A5_strict.ioe"))
+    print("Analyzing ES..", file=sys.stderr)
+    analyze_ES(exons, os.path.join(indir, "_SE_strict.ioe"))
+    print("Analyzing A3..", file=sys.stderr)
+    analyze_SS(exons, os.path.join(indir, "_A3_strict.ioe"))
+    print("Analyzing A5..", file=sys.stderr)
+    analyze_SS(exons, os.path.join(indir, "_A5_strict.ioe"))
 
 
 if __name__ == "__main__":
