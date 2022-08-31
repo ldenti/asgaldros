0456efa269775fc40be71fde2219edb4223f232f
Luca Denti <ldenti92@gmail.com>
2022-08-31 16:36:58 +0200

    Updates script to compute PSI from any BAM - Now supports all SUPPA2 events

--- compute_psi_bam.py
+++ compute_psi_bam.py
@@ -17,9 +17,11 @@ def main():
         "Intron1",
         "Intron2",
         "Intron3",
+        "Intron4",
         "w1",
         "w2",
         "w3",
+        "w4",
         "PSI",
         "OtherIntrons",
         sep=",",
@@ -29,73 +31,111 @@ def main():
             continue
         idx = line.strip("\n").split("\t")[2]
         gene, rest = idx.split(";")
-        _etype, chrom, intron1, intron2, strand = rest.split(":")
-
-        intron1 = (int(intron1.split("-")[0]), int(intron1.split("-")[1]))
-        intron2 = (int(intron2.split("-")[0]), int(intron2.split("-")[1]))
-        intron3 = (0, 0)
         idx = idx.replace(";", "_")
         idx = idx.replace(":", "_")
-        mode = (strand == "+" and _etype == "A5") or (strand == "-" and _etype == "A3")
+        _etype, chrom, *positions, strand = rest.split(":")
+
+        intron1 = (0, 0)
+        intron2 = (0, 0)
+        intron3 = (0, 0)
+        intron4 = (0, 0)
+
         if _etype == "SE":
-            assert (
-                intron1[0] < intron1[1]
-                and intron1[1] < intron2[0]
-                and intron2[0] < intron2[1]
-            )
+            ab, cd = positions
+            intron1 = ab.split("-")
+            intron2 = cd.split("-")
             intron3 = (intron1[0], intron2[1])
-        else:  # A3, A5
+        elif _etype == "A3" or _etype == "A5":
+            ab, cd = positions
+            intron1 = ab.split("-")
+            intron2 = cd.split("-")
+        elif _etype == "RI":
+            a, bc, d = positions
+            intron1 = (b, c)
+        elif _etype == "AF" or _etype == "AL":
+            mode = (strand == "+" and _etype == "AF") or (
+                strand == "-" and _etype == "AL"
+            )
             if mode:
-                assert intron1[1] == intron2[1]
+                a, bc, d, ef = positions
+                b, c = bc.split("-")
+                e, f = ef.split("-")
+                intron1 = (b, c)
+                intron2 = (e, f)
             else:
-                assert intron1[0] == intron2[0]
-            intron1_tmp = (
-                intron1
-                if intron1[1] - intron1[0] > intron2[1] - intron2[0]
-                else intron2
-            )
-            intron2_tmp = (
-                intron2
-                if intron1[1] - intron1[0] > intron2[1] - intron2[0]
-                else intron1
-            )
-            intron1 = intron1_tmp
-            intron2 = intron2_tmp
+                ab, c, de, f = positions
+                a, b = ab.split("-")
+                d, e = de.split("-")
+                intron1 = (a, b)
+                intron2 = (d, e)
+        elif _etype == "MX":
+            ab, cd, ef, gh = positions
+            intron1 = ab.split("-")
+            intron2 = cd.split("-")
+            intron3 = ef.split("-")
+            intron4 = gh.split("-")
+
+        intron1 = [int(p) for p in intron1]
+        intron2 = [int(p) for p in intron2]
+        intron3 = [int(p) for p in intron3]
+        intron4 = [int(p) for p in intron4]
 
         bam_introns = bam.find_introns(
             (
                 read
                 for read in bam.fetch(
                     chrom,
-                    min(intron1[0], intron2[0]) - 50,
-                    max(intron1[1], intron2[1]) + 50,
+                    min(intron1[0] if intron1[0] != 0 else float("inf"), intron2[0] if intron2[0] != 0 else float("inf"), intron3[0] if intron3[0] != 0 else float("inf"), intron4[0] if intron4[0] != 0 else float("inf")) - 50,
+                    max(intron1[1], intron2[1], intron3[1], intron4[1]) + 50,
                 )
             )
         )
-        w1, w2, w3 = 0, 0, 0
+
+        w1, w2, w3, w4 = 0, 0, 0, 0
         new_introns = {}
         for (s, e), w in bam_introns.items():
             e += 1  # CHECKME why do we need this?
-            if (s, e) == intron1:
+            if [s, e] == intron1:
                 w1 = w
-            elif (s, e) == intron2:
+            elif [s, e] == intron2:
                 w2 = w
-            elif (s, e) == intron3:
+            elif [s, e] == intron3:
                 w3 = w
+            elif [s, e] == intron4:
+                w4 = w
             else:
                 new_introns[(s, e)] = w
+        if _etype == "RI":
+            for al in bam.fetch(chrom, intron1[0], intron1[1]):
+                if intron1[0] <= al.reference_start and al.reference_end <= intron1[1]:
+                    w2 += 1
 
         PSI = 0
         if _etype == "SE":
-            if w1 == 0 and w2 == 0 and w3 == 0:
+            if w1 + w2 + w3 == 0:
                 PSI = -1
             else:
                 PSI = ((w1 + w2) / 2) / ((w1 + w2) / 2 + w3)
-        else:  # A3, A5
-            if w1 == 0 and w2 == 0:
+        elif _etype == "A3" or _etype == "A5":
+            if w1 + w2 == 0:
                 PSI = -1
             else:
                 PSI = w1 / (w1 + w2)
+        elif _etype == "RI":
+            if w1 + w2 == 0:
+                PSI = -1
+            else:
+                PSI = w1 / (w1 + w2)
+        elif _etype == "AF" or _etype == "AL":
+            if w1 + w2 == 0:
+                PSI = -1
+            else:
+                PSI = w1 / (w1 + w2)
+        elif _etype == "MX":
+            if w1 + w2 + w3 + w4 == 0:
+                PSI = -1
+            else:
+                PSI = (w1 + w2) / (w1 + w2 + w3 + w4)
         PSI = round(PSI, 3)
 
         print(
@@ -103,12 +143,14 @@ def main():
             chrom,
             _etype,
             f"{intron1[0]}-{intron1[1]}",
-            f"{intron2[0]}-{intron2[1]}",
-            f"{intron3[0]}-{intron3[1]}" if _etype == "SE" else ".",
+            f"{intron2[0]}-{intron2[1]}" if intron2 != [0, 0] else ".",
+            f"{intron3[0]}-{intron3[1]}" if intron3 != [0, 0] else ".",
+            f"{intron4[0]}-{intron4[1]}" if intron4 != [0, 0] else ".",
             w1,
             w2,
-            w3 if _etype == "SE" else -1,
-            PSI,
+            w3,
+            w4,
+            PSI if PSI != -1 else "NaN",
             "/".join(f"{s}-{e}:{w}" for (s, e), w in new_introns.items()),
             sep=",",
         )
